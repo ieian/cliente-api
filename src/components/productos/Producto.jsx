b185c58ec81a2ae7792d463198d17b9f7c5cd038
ieian <yeyotadeoae@gmail.com>
2024-11-21 13:31:14 -0700

    420 - ya se despliegan los productos correctamente

--- src/components/productos/Producto.jsx
+++ src/components/productos/Producto.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Producto() {
+
+function Producto({producto}) {
+    const {_id, nombre, precio, imagen} =producto;
+
+    const eliminarProducto = id => {
+
+    }
+
     return(
-        <h2>Productos</h2>
+        <li className="producto">
+                    <div className="info-producto">
+                        <p className="nombre">{nombre}</p>
+                        <p className="precio">$ {precio}</p>
+                        { imagen ? (
+                            <img src={`http://localhost:5000/${imagen}`} />
+                        ) : null }
+                    </div>
+                    <div className="acciones">
+                        <Link to={`/productos/editar/${_id}`} className="btn btn-azul">
+                            <i className="fas fa-pen-alt"></i>
+                            Editar Producto
+                        </Link>
+
+                        <button type="button" className="btn btn-rojo btn-eliminar" onClick={() => eliminarProducto(_id)}>
+                            <i className="fas fa-times"></i>
+                            Eliminar Cliente
+                        </button>
+                    </div>
+                </li>
     )
 }
 export default Producto;
