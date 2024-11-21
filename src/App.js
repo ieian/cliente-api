c6e7aa1c3809aa81e869fb971ee1ad1fbf1c46c8
ieian <yeyotadeoae@gmail.com>
2024-11-20 18:42:34 -0700

    401 - funciones de header y navegacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
 
+// Routing
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Layaout
+import Header from "./components/layout/Header";
+import Navegacion from "./components/layout/Navegacion";
+
+// Componentes
+////import Clientes from "./components/clientes/Clientes";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <Fragment>
+        <Header />
+        <div className="grid contenedor contenido-principal">
+          <Navegacion />
+
+          <main className="caja-contenido col-9">
+         
+          </main>
+        </div>
+      </Fragment>
+    
+  )
 }
 
 export default App;
