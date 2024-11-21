997bd1f9833e40564df20bc1e6679dde0bd43388
ieian <yeyotadeoae@gmail.com>
2024-11-20 21:23:38 -0700

    410 - llenado de los datos del formulario

--- src/components/clientes/NuevoCliente.js
+++ src/components/clientes/NuevoCliente.js
@@ -1,6 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function NuevoCliente() {
+    const [cliente, guardarClientes] = useState({
+        nombre: '',
+        apellido: '',
+        empresa: '',
+        email: '',
+        telefono: ''
+    });
+
+    const actualizarState = e => {
+        guardarClientes({
+            ...cliente, [e.target.name] : e.target.value
+        })
+        console.log(cliente);
+    }
+
     return (
         <Fragment>
             <h2>Nuevo Cliente</h2>
@@ -10,27 +25,27 @@ function NuevoCliente() {
 
                 <div className="campo">
                     <label>Nombre:</label>
-                    <input type="text" placeholder="Nombre Cliente" name="nombre" />
+                    <input type="text" placeholder="Nombre Cliente" name="nombre" onChange={actualizarState}/>
                 </div>
 
                 <div className="campo">
                     <label>Apellido:</label>
-                    <input type="text" placeholder="Apellido Cliente" name="apellido" />
+                    <input type="text" placeholder="Apellido Cliente" name="apellido" onChange={actualizarState}/>
                 </div>
 
                 <div className="campo">
                     <label>Empresa:</label>
-                    <input type="text" placeholder="Empresa Cliente" name="empresa" />
+                    <input type="text" placeholder="Empresa Cliente" name="empresa" onChange={actualizarState}/>
                 </div>
 
                 <div className="campo">
                     <label>Email:</label>
-                    <input type="email" placeholder="Email Cliente" name="email" />
+                    <input type="email" placeholder="Email Cliente" name="email" onChange={actualizarState}/>
                 </div>
 
                 <div className="campo">
                     <label>Teléfono:</label>
-                    <input type="email" placeholder="Teléfono Cliente" name="telefono" />
+                    <input type="email" placeholder="Teléfono Cliente" name="telefono" onChange={actualizarState}/>
                 </div>
 
                 <div className="enviar">
