097dd56ee33572e9c055572721865005351be81c
ieian <yeyotadeoae@gmail.com>
2024-11-21 14:32:26 -0700

    424 - se leen los datos del state y se cambio un class por className

--- src/components/productos/NuevoProducto.jsx
+++ src/components/productos/NuevoProducto.jsx
@@ -14,6 +14,17 @@ function NuevoProducto() {
 
     const [archivo, guardarArchivo] = useState('');
 
+    const leerInfoProducto = e => {
+        guardarProducto({
+            ...producto,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const leerArchivo = e => {
+        guardarArchivo( e.target.files[0] );
+    }
+
     return(
         <Fragment>
             <h2>Nuevo Producto</h2>
@@ -23,17 +34,17 @@ function NuevoProducto() {
 
                 <div className="campo">
                     <label>Nombre:</label>
-                    <input type="text" placeholder="Nombre Producto" name="nombre" />
+                    <input type="text" placeholder="Nombre Producto" name="nombre" onChange={leerInfoProducto}/>
                 </div>
 
                 <div className="campo">
                     <label>Precio:</label>
-                    <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" />
+                    <input type="number" name="precio" min="0.00" step="1" placeholder="Precio" onChange={leerInfoProducto}/>
                 </div>
 
                 <div className="campo">
                     <label>Imagen:</label>
-                    <input type="file"  name="imagen" />
+                    <input type="file"  name="imagen" onChange={leerArchivo}/>
                 </div>
 
                 <div className="enviar">
--- src/components/productos/Producto.jsx
+++ src/components/productos/Producto.jsx
@@ -34,25 +34,25 @@ function Producto({producto}) {
 
     return(
         <li className="producto">
-                    <div className="info-producto">
-                        <p className="nombre">{nombre}</p>
-                        <p className="precio">$ {precio}</p>
-                        { imagen ? (
-                            <img src={`http://localhost:5000/${imagen}`} />
-                        ) : null }
-                    </div>
-                    <div className="acciones">
-                        <Link to={`/productos/editar/${_id}`} className="btn btn-azul">
-                            <i className="fas fa-pen-alt"></i>
-                            Editar Producto
-                        </Link>
+            <div className="info-producto">
+                <p className="nombre">{nombre}</p>
+                <p className="precio">$ {precio}</p>
+                { imagen ? (
+                    <img src={`http://localhost:5000/${imagen}`} />
+                ) : null }
+            </div>
+            <div className="acciones">
+                <Link to={`/productos/editar/${_id}`} className="btn btn-azul">
+                    <i className="fas fa-pen-alt"></i>
+                    Editar Producto
+                </Link>
 
-                        <button type="button" className="btn btn-rojo btn-eliminar" onClick={() => eliminarProducto(_id)}>
-                            <i className="fas fa-times"></i>
-                            Eliminar Producto
-                        </button>
-                    </div>
-                </li>
+                <button type="button" className="btn btn-rojo btn-eliminar" onClick={() => eliminarProducto(_id)}>
+                    <i className="fas fa-times"></i>
+                    Eliminar Producto
+                </button>
+            </div>
+        </li>
     )
 }
 export default Producto;
