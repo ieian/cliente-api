12a30dea446511f3b6826a028bb7ead87dcbaf7f
ieian <yeyotadeoae@gmail.com>
2024-11-20 20:54:16 -0700

    407 - desplegar clientes en la pantalla principal

--- src/components/clientes/Clientes.js
+++ src/components/clientes/Clientes.js
@@ -1,18 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import clienteAxios from '../../config/axios';
+import Cliente from './Cliente';
 
 function Clientes() {
+    const [clientes, guardarClientes] = useState([]);
+
     const consultarAPI = async () => {
         const clientesConsulta = await clienteAxios.get('/clientes');
-        
-        console.log(clientesConsulta.data); 
+
+        guardarClientes(clientesConsulta.data);
     };
 
     useEffect(() => {
         consultarAPI();
     }, []); 
 
-    return <h2>Clientes</h2>;
+    return (
+        <Fragment>
+            <h2>Clientes</h2>
+
+            <ul className='listado-clientes'>
+                {clientes.map(cliente => (
+                    <Cliente 
+                        key={cliente._id}
+                        cliente = {cliente}
+                    />
+                ))}
+            </ul>
+        </Fragment>
+    )
 }
 
 export default Clientes;
