e78901ac1b626f9f5c4e19c85377b22336c3b1dc
ieian <yeyotadeoae@gmail.com>
2024-11-22 00:37:06 -0700

    440 - ventana de pedidos

--- src/components/pedidos/Pedidos.jsx
+++ src/components/pedidos/Pedidos.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+import { useNavigate, useParams } from 'react-router-dom';
+import clienteAxios from '../../config/axios';
+
+import DetallesPedido from './DetallesPedido';
 
 function Pedidos() {
+    const [pedidos, guardarPedidos] = useState([]);
+
+    useEffect(() =>{
+        const consultarAPI = async () => {
+            const resultado = await clienteAxios.get('/pedidos');
+    
+            guardarPedidos(resultado.data);
+        };
+
+        consultarAPI();
+    }, []);
+
     return(
-        <h2>Pedidos</h2>
+        <Fragment>
+            <h2>Pedidos</h2>
+
+            <ul className="listado-pedidos">
+                {pedidos.map(pedido => (
+                    <DetallesPedido
+                        key={pedido._id}
+                        pedido={pedido}
+                    />
+                ))}
+            </ul>
+        </Fragment>
     )
 }
 export default Pedidos;
