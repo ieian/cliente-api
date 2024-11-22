fdc365eeceed444b1abca39f8340fa2ab33057cf
ieian <yeyotadeoae@gmail.com>
2024-11-22 13:52:38 -0700

    451 -  cerrar sesion

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Header = () => (
-    <header className="barra">
-        <div className="contenedor">
-            <h1>CRM - Administrador de Clientes</h1>
-        </div>
-    </header>
-);
+import { CRMContext } from "../../context/CRMContext";
+
+const Header = () => {
+    const history = useNavigate();
+
+    const [auth, guardarAuth] =useContext(CRMContext);
+
+    const cerrarSesion = () => {
+        guardarAuth({
+            token: '',
+            auth: false
+        });
+
+        localStorage.setItem('token', '');
+
+        history('/iniciar-sesion');
+    }
+
+    return(
+        <header className="barra">
+            <div className="contenedor">
+                <div className="contenido-barra">
+                    <h1>YEYO - Administrador de Clientes</h1>
+                    { auth.auth ? (
+                        <button type="button" className="btn btn-rojo" onClick={cerrarSesion}>
+                            <i className="far fa-times-circle"></i>
+                            Cerrar Sesion
+                        </button>
+                    ) : null }
+                </div>
+            </div>
+        </header>
+    )
+}
 
 export default Header;
--- src/components/layout/Navegacion.js
+++ src/components/layout/Navegacion.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CRMContext } from "../../context/CRMContext";
 
-const Navegacion = () => (
-    <aside className="sidebar col-3">
+const Navegacion = () => {
+    const [auth, guardarAuth] =useContext(CRMContext);
+
+    if(!auth.auth) return null;
+
+    return(
+        <aside className="sidebar col-3">
         <h2>Administración</h2>
 
         <nav className="navegacion">
@@ -11,7 +17,7 @@ const Navegacion = () => (
             <Link to={"/pedidos"} className="pedidos">Pedidos</Link>
         </nav>
     </aside> 
-);
-
+    )
+}
  
 export default Navegacion;
